09f59f6a3246dbfdbb2d3167618881cb06e50b89
Jordan Rhodes <jsrhodes15@gmail.com>
2019-06-13 19:17:24 -0400

    Complete code coverage section of Arrays and Slices

--- sum/sum_test.go
+++ sum/sum_test.go
@@ -4,10 +4,11 @@ import "testing"
 
 func TestSum(t *testing.T) {
 
-	t.Run("collection of 5 numbers", func(t *testing.T) {
-		numbers := []int{1, 2, 3, 4, 5}
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
 		got := Sum(numbers)
-		want := 15
+		want := 6
 
 		if got != want {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
